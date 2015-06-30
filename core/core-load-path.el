54b63231ae9946a8f55499cb4a4127b2ec5731e8
sawada_yasuharu <sawada_yasuharu@cyberagent.co.jp>
2015-06-30 15:58:58 +0900

    Updated load-paths.

--- core/core-load-path.el
+++ core/core-load-path.el
@@ -1,29 +1,29 @@
 (defun add-to-load-path (dir) (add-to-list 'load-path dir))
 
 ;; path
-(defconst personal-core-directory
+(defconst baymacs-core-directory
   (expand-file-name (concat user-emacs-directory "core/"))
   "設定ファイルのコアディレクトリ")
-(defconst personal-banner-directory
-  (expand-file-name (concat personal-core-directory "banners/"))
-  "Personal banners directory.")
-(defconst personal-banner-default-png
-  (expand-file-name (concat personal-banner-directory "img/spacemacs.png"))
-  "Personal default banner image.")
-(defconst personal-directory
-  (expand-file-name (concat user-emacs-directory "personal/"))
-  "Personal base directory.")
-(defconst personal-cache-directory
+(defconst baymacs-banner-directory
+  (expand-file-name (concat baymacs-core-directory "banners/"))
+  "Baymacs banners directory.")
+(defconst baymacs-banner-default-png
+  (expand-file-name (concat baymacs-banner-directory "img/baymacs.png"))
+  "Baymacs default banner image.")
+(defconst baymacs-directory
+  (expand-file-name (concat user-emacs-directory "baymacs/"))
+  "Baymacs base directory.")
+(defconst baymacs-cache-directory
   (expand-file-name (concat user-emacs-directory ".cache/"))
-  "Personal storage area for persistent files")
+  "Baymacs storage area for persistent files")
 
 (defconst user-home-directory
   (expand-file-name "~/")
   "User home directory (~/).")
 (defconst pcache-directory
-  (concat personal-cache-directory "pcache"))
-(unless (file-exists-p personal-cache-directory)
-  (make-directory personal-cache-directory))
+  (concat baymacs-cache-directory "pcache"))
+(unless (file-exists-p baymacs-cache-directory)
+  (make-directory baymacs-cache-directory))
 
 (defconst user-dropbox-directory
   (expand-file-name (concat user-home-directory "Dropbox/"))
@@ -31,7 +31,7 @@
 
 (mapc 'add-to-load-path
   `(
-    ,personal-core-directory
+    ,baymacs-core-directory
 ;;        ,(concat user-emacs-directory "core/libs/")
 ;;        ,(concat user-dropbox-directory "emacs/")
   ))
