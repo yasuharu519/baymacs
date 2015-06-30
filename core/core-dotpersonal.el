71fcf05c2a387a6f5bac1c50c7f41eeaf558cb40
sawada_yasuharu <sawada_yasuharu@cyberagent.co.jp>
2015-06-30 15:59:56 +0900

    Delete unneeded file.

--- core/core-dotpersonal.el
+++
@@ -1,8 +0,0 @@
-(defmacro dotpersonal|call-func (func &optional msg)
-  "関数が定義されている場合は関数呼び出し。
-  MSGがnilでない場合は `*Messages' に表示する."
-  `(progn
-     (when ,msg (personal-buffer/message ,msg))
-     (if (fboundp ',func) (,func))))
-
-(provide 'core-dotpersonal)
