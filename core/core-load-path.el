d5dc42f9351fed93b3ff34cb2d2f9b22cd7bf917
Yasuharu Sawada <yasuharu519@gmail.com>
2015-07-05 22:48:17 +0900

    Added

--- core/core-load-path.el
+++ core/core-load-path.el
@@ -16,6 +16,15 @@
 (defconst baymacs-cache-directory
   (expand-file-name (concat user-emacs-directory ".cache/"))
   "Baymacs storage area for persistent files")
+(defconst baymacs-package-directory
+  (expand-file-name (concat user-emacs-directory "el-get/"))
+  "Baymacs directory which contains packages")
+(defconst baymacs-package-manager-directory
+  (expand-file-name (concat baymacs-package-directory "el-get/"))
+  "Baymacs package manager directory")
+(defconst baymacs-personal-elisps-directory
+  (expand-file-name (concat baymacs-package-directory "elisps/"))
+  "Baymacs package manager directory")
 
 (defconst user-home-directory
   (expand-file-name "~/")
@@ -29,9 +38,25 @@
   (expand-file-name (concat user-home-directory "Dropbox/"))
   "Dropbox directory.")
 
+(defconst user-backup-directory
+  (expand-file-name (concat user-emacs-directory "backup"))
+  "User backup directory.")
+(unless (file-exists-p user-backup-directory)
+  (make-directory user-backup-directory))
+
+;; バックバップの保存先変更
+(setq backup-directory-alist
+      (cons (cons ".*" user-backup-directory)
+	    backup-directory-alist))
+;; 自動保存ファイルの保存先変更
+(setq auto-save-file-name-transforms
+      `((".*" ,user-backup-directory t)))
+
 (mapc 'add-to-load-path
   `(
     ,baymacs-core-directory
+    ,baymacs-package-manager-directory
+    ,baymacs-personal-elisps-directory
 ;;        ,(concat user-emacs-directory "core/libs/")
 ;;        ,(concat user-dropbox-directory "emacs/")
   ))
--- init.el
+++ init.el
@@ -2,10 +2,22 @@
 (defconst baymacs-emacs-min-version   "24.3" "Minimal version of Emacs.")
 
 (defun baymacs/emacs-version-ok ()
-  (version<= personal-emacs-min-version emacs-version))
+  (version<= baymacs-emacs-min-version emacs-version))
+
+;; evilの設定
+;; http://d.hatena.ne.jp/tarao/20130304/evil_config
+(setq evil-want-C-u-scroll t
+      evil-search-module 'evil-search
+      evil-ex-search-vim-style-regexp t)
 
 (when (baymacs/emacs-version-ok)
   ;; ロードパスのロード
   (load-file (concat user-emacs-directory "core/core-load-path.el"))
+  (require 'core-package-manager)
+  (baymacs/core-package-manager-init)
+  (baymacs/core-package-manager-install-packages)
+
+  (require 'core-baymacs)
   )
+
 
