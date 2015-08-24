056a0a080e4baf5d1ff36d84114a891a0731ebcc
sawada_yasuharu <sawada_yasuharu@cyberagent.co.jp>
2015-08-24 12:53:51 +0900

    Changed init.el settings.

--- init.el
+++ init.el
@@ -20,11 +20,15 @@
   (load-theme 'monokai t t)
   (enable-theme 'monokai)
 
+  ;; 環境変数を引き継ぐ
+  (when (memq window-system '(mac ns))
+    (exec-path-from-shell-initialize))
+
   ;; init-loader
   (require 'init-loader)
   (custom-set-variables
     '(init-loader-byte-compile t)
-    ;;'(init-loader-show-log-after-init 'error-only) ;; エラーだけ表示
+    '(init-loader-show-log-after-init 'error-only) ;; エラーだけ表示
     )
   (init-loader-load (concat user-emacs-directory "init-loader"))
 
